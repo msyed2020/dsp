92d69adcce37e70a506db83d11a982a498c25b76
Mikail <mikailsyed@yahoo.com>
2025-07-20 22:17:52 -0400

    signal mean

--- signal_mean/main.c
+++ signal_mean/main.c
@@ -9,5 +9,10 @@ int main() {
 }
 
 double calcSignalMean(double *signalSrcArr, int sigLength) {
-    
+    double mean = 0.0;
+    for (int i = 0; i < sigLength; i++) {
+        mean += signalSrcArr[i];
+    }
+    mean = mean / (double) sigLength;
+    return mean;
 }
