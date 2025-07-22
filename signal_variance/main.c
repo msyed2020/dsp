3e04658f44014759a24b2de7bb218c78266c2ae1
Mikail <mikailsyed@yahoo.com>
2025-07-22 11:24:37 -0400

    variance algo

--- signal_variance/main.c
+++ signal_variance/main.c
@@ -4,12 +4,16 @@
 
 extern double InputSignal_f32_1kHz_15kHz[SIG_LENGTH];
 double calcSignalMean(double *signalSrcArr, int sigLength);
+double calcSignalVariance(double *signalSrcArr, double sigMean, int sigLength);
 double avg;
+double vari;
 
 int main() {
 
     avg = calcSignalMean(&InputSignal_f32_1kHz_15kHz[0], SIG_LENGTH);
+    vari = calcSignalVariance(&InputSignal_f32_1kHz_15kHz[0], avg, SIG_LENGTH);
     printf("Mean = %f\n", avg);
+    printf("Average = %f\n", vari);
     return 0;
 }
 
@@ -21,3 +25,12 @@ double calcSignalMean(double *signalSrcArr, int sigLength) {
     mean = mean / (double) sigLength;
     return mean;
 }
+
+double calcSignalVariance(double *signalSrcArr, double sigMean, int sigLength) {
+    double variance = 0.0;
+    for (int i = 0; i < sigLength; i++) {
+        variance += pow((signalSrcArr[i] - sigMean), 2);
+    }
+    variance = variance / (sigLength - 1);
+    return variance;
+}  
