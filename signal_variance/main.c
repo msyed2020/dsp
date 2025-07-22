117c92c7d21fd4c77959e14c008410d56d6e0d17
Mikail <mikailsyed@yahoo.com>
2025-07-22 11:18:00 -0400

    sig wave

---
+++ signal_variance/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIG_LENGTH 320
+
+extern double InputSignal_f32_1kHz_15kHz[SIG_LENGTH];
+double calcSignalMean(double *signalSrcArr, int sigLength);
+double avg;
+
+int main() {
+
+    avg = calcSignalMean(&InputSignal_f32_1kHz_15kHz[0], SIG_LENGTH);
+    printf("Mean = %f\n", avg);
+    return 0;
+}
+
+double calcSignalMean(double *signalSrcArr, int sigLength) {
+    double mean = 0.0;
+    for (int i = 0; i < sigLength; i++) {
+        mean += signalSrcArr[i];
+    }
+    mean = mean / (double) sigLength;
+    return mean;
+}
