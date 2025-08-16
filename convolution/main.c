bbdfb08218e04757a7dac87e78006968fe4ecee7
Mikail <mikailsyed@yahoo.com>
2025-08-16 10:44:48 -0400

    lol

--- convolution/main.c
+++ convolution/main.c
@@ -2,6 +2,8 @@
 
 // Linear convolution algorithm
 
+// waveforms.c is the input signal
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -12,34 +14,6 @@ extern double InputSignal_f32_1kHz_15kHz[SIG_LENGTH];
 
 int main() {
 
-    // avg = calcSignalMean(&InputSignal_f32_1kHz_15kHz[0], SIG_LENGTH);
-    // vari = calcSignalVariance(&InputSignal_f32_1kHz_15kHz[0], avg, SIG_LENGTH);
-    // standardDev = calcSignalSTD(vari);
-    // printf("Mean = %f\n", avg);
-    // printf("Average = %f\n", vari);
-    // printf("Standard Deviation = %f\n", standardDev);
     return 0;
 }
 
-double calcSignalMean(double *signalSrcArr, int sigLength) {
-    double mean = 0.0;
-    for (int i = 0; i < sigLength; i++) {
-        mean += signalSrcArr[i];
-    }
-    mean = mean / (double) sigLength;
-    return mean;
-}
-
-double calcSignalVariance(double *signalSrcArr, double sigMean, int sigLength) {
-    double variance = 0.0;
-    for (int i = 0; i < sigLength; i++) {
-        variance += pow((signalSrcArr[i] - sigMean), 2);
-    }
-    variance = variance / (sigLength - 1);
-    return variance;
-}  
-
-double calcSignalSTD(double sigVar) {
-    double std = sqrt(sigVar);
-    return std;
-}
