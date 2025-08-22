47545261a7892b2418813abc04e29da929ba17d7
Mikail <mikailsyed@yahoo.com>
2025-08-22 09:45:44 -0400

    done

--- running sum/main.c
+++ running sum/main.c
@@ -8,28 +8,21 @@
 #include <stdlib.h>
 #include <math.h>
 #define SIG_LENGTH 320
-#define IMP_RSP 29
 
 extern double InputSignal_f32_1kHz_15kHz[SIG_LENGTH];
-double output_sig[SIG_LENGTH + IMP_RSP];
+double output_sig[SIG_LENGTH];
 
 
 int main() {
 
-    FILE *inputSigFPTR, *impRspFPTR, *outputSigFPTR;
+    FILE *inputSigFPTR, *outputSigFPTR;
     inputSigFPTR = fopen("input_signal.dat", "w");
-    impRspFPTR = fopen("impulse_response.dat", "w");
     outputSigFPTR = fopen("output_signal.dat", "w");
 
     for (int i = 0; i < SIG_LENGTH; i++) {
         fprintf(inputSigFPTR, "\n%f", InputSignal_f32_1kHz_15kHz[i]);
     }
     fclose(inputSigFPTR);
-
-    for (int i = 0; i < IMP_RSP; i++) {
-        fprintf(impRspFPTR, "\n%f", Impulse_response[i]);
-    }
-    fclose(impRspFPTR);
 
     for (int i = 0; i < SIG_LENGTH + IMP_RSP; i++) {
         fprintf(outputSigFPTR, "\n%f", output_sig[i]);
