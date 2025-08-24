d4b8c744e2c252db5ad57170e1d966ec6e7a5373
Mikail <mikailsyed@yahoo.com>
2025-08-24 12:24:57 -0400

    func

--- DFT/main.c
+++ DFT/main.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define SIG_LENGTH 320
+
+extern double InputSignal_f32_1kHz_15kHz[SIG_LENGTH];
+void calcDFT(double * sigSrcArr, double * sigDestReXArr, double * sigDestImXArr, int sig_length);
 
 int main() {
 
-    
+
 
     return 0;
+}
+
+void calcDFT(double * sigSrcArr, double * sigDestReXArr, double * sigDestImXArr, int sig_length) {
+    int i, j;
+    
 }
