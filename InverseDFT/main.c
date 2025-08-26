e07b1f7164effa14b5093dc5212a1659e95e7247
Mikail <mikailsyed@yahoo.com>
2025-08-26 19:44:02 -0400

    func dec

--- InverseDFT/main.c
+++ InverseDFT/main.c
@@ -1,4 +1,4 @@
-// compile: gcc main.c waveforms.c -o dft -lm
+// compile: gcc main.c waveforms.c -o idft -lm
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,7 +8,7 @@
 extern double InputSignal_f32_1kHz_15kHz[SIG_LENGTH];
 double outputReX[SIG_LENGTH / 2];
 double outputImX[SIG_LENGTH / 2];
-void calcDFT(double * sigSrcArr, double * sigDestReXArr, double * sigDestImXArr, int sig_length);
+void calcInverseDFT(double *IDFTOutArr, double *sigSrcReXArr, double *sigSrcImXArr, int IDFTLength);
 
 int main() {
 
@@ -35,19 +35,6 @@ int main() {
     return 0;
 }
 
-void calcDFT(double * sigSrcArr, double * sigDestReXArr, double * sigDestImXArr, int sig_length) {
-    int i, j, k;
-    double PI = 3.14159265359;
-
-    for (j = 0; j < sig_length / 2; j++) {
-        sigDestReXArr[j] = 0;
-        sigDestImXArr[j] = 0;
-    }
-
-    for (k = 0; k < (sig_length / 2); k++) {
-        for (i = 0; i < sig_length; i++) {
-            sigDestReXArr[k] += sigSrcArr[i]*cos(2 * PI * k * i / sig_length);
-            sigDestImXArr[k] -= sigSrcArr[i]*sin(2 * PI * k * i / sig_length);
-        }
-    }
+void calcInverseDFT(double *IDFTOutArr, double *sigSrcReXArr, double *sigSrcImXArr, int IDFTLength) {
+    
 }
