61ea47563e489e8ccb2e89bf9896f9e1feb3f220
Mikail <mikailsyed@yahoo.com>
2025-08-30 14:00:24 -0400

    idft

--- InverseDFT/main.c
+++ InverseDFT/main.c
@@ -15,10 +15,6 @@ double outputIDFT[SIG_LENGTH];
 void calcDFT(double * sigSrcArr, double * sigDestReXArr, double * sigDestImXArr, int sig_length);
 void calcInverseDFT(double *IDFTOutArr, double *sigSrcReXArr, double *sigSrcImXArr, int IDFTLength);
 
-
-
-
-
 int main() {
 
     FILE *fptr, *fptr2, *fptr3, *fptr4;
@@ -89,3 +85,6 @@ void calcInverseDFT(double *IDFTOutArr, double *sigSrcReXArr, double *sigSrcImXA
         }
     }
 }
+
+
+
