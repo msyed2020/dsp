de5f6175b656f53a83e2caaf1418829ee8557bfc
msyed2020 <mikailsyed@yahoo.com>
2025-09-13 15:38:42 -0400

    func writ

--- RectToPolar/main.c
+++ RectToPolar/main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define SIG_LENGTH 320
 
-extern double _320_pts_ecg_IMX[320];
-extern double _320_pts_ecg_REX[320];
+extern double _320_pts_ecg_IMX[SIG_LENGTH];
+extern double _320_pts_ecg_REX[SIG_LENGTH];
 
 int main() {
 
-    
+
 
     return 0;
+}
+
+void rectToPolarConversion(double * sigSrcREX, double * sigSrcIMX,
+                            double * sigOutMAG, double * sigOutPhase,
+                            int sigLength)
+{
+    
 }
