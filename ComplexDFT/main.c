50f0a84428ded0af2f2670cb65ccb6adeca0c974
msyed2020 <mikailsyed@yahoo.com>
2025-09-23 19:23:16 -0400

    rtp del

--- ComplexDFT/main.c
+++ ComplexDFT/main.c
@@ -1,26 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#define SIG_LENGTH 320
+#define SIG_LENGTH 501
 
 // compile: compile: gcc main.c waveforms.c -o recttopolar -lm
 
-extern double _320_pts_ecg_IMX[SIG_LENGTH];
-extern double _320_pts_ecg_REX[SIG_LENGTH];
+extern double _501pts_20Hz_sig_imx[SIG_LENGTH];
+extern double _501pts_20Hz_sig_rex[SIG_LENGTH];
 
 double OutputMAG[SIG_LENGTH];
 double OutputPhase[SIG_LENGTH];
 
-void rectToPolarConversion(double * sigSrcREX, double * sigSrcIMX,
-                            double * sigOutMAG, double * sigOutPhase,
-                            int sigLength);
 
 int main() {
 
     FILE *inputREXfptr, *inputIMXfptr, *outputMAGfptr, *outputPhasefptr;
-    rectToPolarConversion((double *) &_320_pts_ecg_REX[0], (double *) &_320_pts_ecg_IMX[0],
-                            (double *) &OutputMAG[0], (double *) &OutputPhase[0],
-                            (int) SIG_LENGTH);
 
     inputREXfptr = fopen("input_rex.dat", "w");
     inputIMXfptr = fopen("input_imx.dat", "w");
@@ -40,31 +34,4 @@ int main() {
     fclose(outputPhasefptr);
 
     return 0;
-}
-
-void rectToPolarConversion(double * sigSrcREX, double * sigSrcIMX,
-                            double * sigOutMAG, double * sigOutPhase,
-                            int sigLength)
-{
-    double PI = 3.14159265358979f;
-    int k;
-    for (k = 0; k < sigLength; k++) {
-
-        sigOutMAG[k] = sqrt(powf(sigSrcREX[k], 2) + powf(sigSrcIMX[k], 2));
-        
-        if (sigSrcREX[k] == 0) {
-            sigSrcREX[k] = pow(10, -20);
-            sigOutPhase[k] = atan(sigSrcIMX[k] / sigSrcREX[k]);
-        }
-
-        if ((sigSrcREX[k] < 0) && (sigSrcIMX[k] < 0)) {
-            sigOutPhase[k] -= PI;
-        }
-
-        if ((sigSrcREX[k] < 0) && (sigSrcIMX[k] >= 0)) {
-            sigOutPhase[k] += PI;
-        }
-
-
-    }
 }
