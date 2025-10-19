40c0b0a67da64707205d865bdf37a3a33b7a6242
msyed2020 <mikailsyed@yahoo.com>
2025-10-19 09:46:33 -0400

    dsp

--- LowPassWindowedSincFilter/main.c
+++ LowPassWindowedSincFilter/main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+/*
+
+Cutoff frequency is normalized. Must be between
+0 and 0.5, where 0.5 represents the Nyquist frequency
+
+*/
 
 int main() {
-    printf("Hello World");
-    
 
     return 0;
 }
