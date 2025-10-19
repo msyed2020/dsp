54f7194469b734e753d1e489c5a8d16a66d45f10
msyed2020 <mikailsyed@yahoo.com>
2025-10-19 09:56:13 -0400

    func dec

--- LowPassWindowedSincFilter/main.c
+++ LowPassWindowedSincFilter/main.c
@@ -13,3 +13,15 @@ int main() {
 
     return 0;
 }
+
+void lowPassWindowSincFilter(double *sig_src_arr,
+                            double *sig_dest_arr,
+                            double *fltr_kernel_dest,
+                            double cutoffFreq,
+                            int filterLength,
+                            int inputSigLength)
+{
+
+    
+
+}
