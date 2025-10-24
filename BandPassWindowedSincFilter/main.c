b921d77dfb070d3e2b47f88c84c6e90e06d4b412
msyed2020 <mikailsyed@yahoo.com>
2025-10-24 13:27:43 -0400

    lpfk

--- BandPassWindowedSincFilter/main.c
+++ BandPassWindowedSincFilter/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 #define SIG_LENGTH  320
 #define KERNEL_LENGTH   29
 
@@ -32,5 +35,16 @@ void bandPassWindowedSinc(
                         double *sigDestArr,
                         int inputSigLength)
 {
-    
+    // Calculating the first low pass filter kernel
+    for (int i = 0; i < filterLength; i++) {
+
+        if (i - filterLength / 2 == 0) {
+            lowerCutoffStateBuff[i] = 2 * M_PI * lowerCutoff;
+        }
+        if (i - filterLength / 2 != 0) {
+            lowerCutoffStateBuff[i] = sin(2 * M_PI * lowerCutoff * (i - filterLength / 2)) / (i - filterLength / 2);
+            lowerCutoffStateBuff[i] *= (0.42 - 0.5*cos(2 * M_PI * i / filterLength) + 0.08*cos(4 * M_PI * i / filterLength));
+        }
+    }
+
 }
