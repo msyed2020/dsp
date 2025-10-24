249da2ab3b64a7af28252ecdfed3df3650f08667
msyed2020 <mikailsyed@yahoo.com>
2025-10-24 13:19:42 -0400

    func dec

--- BandPassWindowedSincFilter/main.c
+++ BandPassWindowedSincFilter/main.c
@@ -3,3 +3,20 @@
 #include <math.h>
 #define SIG_LENGTH  320
 #define KERNEL_LENGTH   29
+
+int main() {
+    
+
+    return 0;
+}
+
+void bandPassWindowedSinc(
+                        double *lowerCutoffStateBuff,
+                        double *upperCutoffStateBuff,
+                        double *filterKernelDestinationArr,
+                        double lowerCutoff,
+                        double upperCutoff,
+                        int filterLength,
+                        double *sigSrcArr,
+                        double *sigDestArr,
+                        int inputSigLength)
